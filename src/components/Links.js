1ec4783f0ccfcfc27718400b72e71a9cd826b190
Kevinmrozek <kmrozek0859@gmail.com>
2023-06-21 22:15:22 -0500

    complete lab

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+
+function Links(props) {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={props.links}>https://github.com/liza</a>
+            <a href={props.links}>https://www.linkedin.com/in/liza/</a>
+        </div>
+    );
+
+}
+export default Links;
